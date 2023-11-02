1407dbb8eaaabd507b1b9f8532f3e86445ddf5c5
JirahGrace <jirah.baker@gmail.com>
2023-11-02 18:18:48 +1030

    Created file, added personal details heading and basic class nams.

---
+++ OOP Assignment 2.py
@@ -0,0 +1,34 @@
+'''
+File: OOP Assignment 2.py
+Description: Main Code File for RPG OOP Assignment 2.
+Author: Jirah Baker
+StudentID: 110332584
+EmailID: bakjg001
+This is my own work as defined by the University's Academic Misconduct Policy.
+'''
+
+class Alchemist:
+    pass
+
+class Laboratory:
+    pass
+
+class Potion:
+    pass
+
+class SuperPotion(Potion):
+    pass
+
+class ExtremePotion(Potion):
+    pass
+
+class Reagent:
+    pass
+
+class Herb(Reagent):
+    pass
+
+class Catalyst(Reagent):
+    pass
+
+
