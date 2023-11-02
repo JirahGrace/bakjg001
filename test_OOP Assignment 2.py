4caaae26d513cc7e0d61a569a2ed2cffb6304233
JirahGrace <jirah.baker@gmail.com>
2023-11-02 18:35:04 +1030

    Created the test file, added the personal details heading.

---
+++ test_OOP Assignment 2.py
@@ -0,0 +1,10 @@
+'''
+File: test_OOP Assignment 2.py
+Description: Test Code File for RPG OOP Assignment 2.
+Author: Jirah Baker
+StudentID: 110332584
+EmailID: bakjg001
+This is my own work as defined by the University's Academic Misconduct Policy.
+'''
+
+import unittest
