1ac656010210d3f8393e1731049140bab1b1ba11
JirahGrace <jirah.baker@gmail.com>
2023-11-07 13:49:42 +1030

    Fixed abstract class titles and created init for Alchemist.

--- OOP Assignment 2.py
+++ OOP Assignment 2.py
@@ -7,13 +7,24 @@ EmailID: bakjg001
 This is my own work as defined by the University's Academic Misconduct Policy.
 '''
 
+from abc import ABC, abstractmethod
+
+
 class Alchemist:
-    pass
+    def __init__(self, attack, strength, defense, magic, ranged, necromancy, laboratory, recipies):
+        self.attack = attack
+        self.strength = strength 
+        self.defense = defense
+        self.magic = magic
+        self.ranged = ranged
+        self.necromancy = necromancy
+        self.laboratory = Laboratory
+        self.recipies = {}
 
 class Laboratory:
     pass
 
-class Potion:
+class Potion(ABC):
     pass
 
 class SuperPotion(Potion):
@@ -22,7 +33,7 @@ class SuperPotion(Potion):
 class ExtremePotion(Potion):
     pass
 
-class Reagent:
+class Reagent(ABC):
     pass
 
 class Herb(Reagent):
