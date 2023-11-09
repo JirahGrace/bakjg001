83d20b098eba7b910d8fff5d80bfe316c5f524cc
JirahGrace <jirah.baker@gmail.com>
2023-11-09 14:35:38 +1030

    Set up names and definitions for methods

--- OOP Assignment 2.py
+++ OOP Assignment 2.py
@@ -21,11 +21,37 @@ class Alchemist:
         self.__laboratory = Laboratory
         self.__recipies = {}
 
+    def getLaboratory(self):
+        pass
+
+    def getRecipies(self):
+        pass
+
+    def mixPotion(self, recipe):
+        pass
+
+    def drinkPotion(self, potion):
+        pass
+
+    def collectReagent(self, reagent, amount):
+        pass
+
+    def refineReagent(self):
+        pass
+
+
 class Laboratory:
     def __init__(self, potions, herbs, catalysts):
         self.__potions = []
         self.__herbs = []
         self.__init__catalysts = []
+    
+    def mixPotion(self, name, type, stat, primaryIngredient, secondaryIngredient):
+        pass
+
+    def addReagent(self, reagent, amount):
+        pass
+
 
 class Potion(ABC):
     def __init__(self, name, stat, boost):
@@ -33,11 +59,37 @@ class Potion(ABC):
         self.__stat = stat
         self.__boost = boost
 
+    @abstractmethod
+    def calculateBoost(self):
+        pass
+
+    def getName(self):
+        pass
+
+    def getStat(self):
+        pass
+
+    def getBoost(self):
+        pass
+
+    def setBoost(self, boost):
+        pass
+
+
 class SuperPotion(Potion):
     def __init__(self, herb, catalyst, name, stat, boost):
         super().__init__(name, stat, boost)
         self.__herb = herb
         self.__cataylst = catalyst
+
+    def caculateBoost(self):
+        pass
+
+    def getHerb(self):
+        pass
+
+    def getCatalyst(self):
+        pass
 
 
 class ExtremePotion(Potion):
@@ -46,19 +98,57 @@ class ExtremePotion(Potion):
         self.__reagent = reagent
         self.__potion = potion
 
+    def calculateBoost(self):
+        pass
+
+    def getReagent(self):
+        pass
+
+    def getPotion(self):
+        pass
+
+
 class Reagent(ABC):
     def __init__(self, name, potency):
         self.__name = name
         self.__potency = potency
+
+    @abstractmethod
+    def refine(self):
+        pass
+
+    def getName(self):
+        pass
+
+    def getPotency(self):
+        pass
+
+    def setPotency(self, potency):
+        pass
+
 
 class Herb(Reagent):
     def __init__(self, grimy, name, potency):
         super().__init__(name, potency)
         self.__grimy = True
 
+    def refine(self):
+        pass
+
+    def getGrimy(self):
+        pass
+
+    def setGrimy(self, grimy):
+        pass
+
+
 class Catalyst(Reagent):
     def __init__(self, quality, name, potency):
         super().__init__(name, potency)
         self.__quality = quality
 
+    def refine(self):
+        pass
 
+    def getQuality(self):
+        pass
